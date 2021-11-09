d9eb0cba4045dcd901afeb994fd4bb8ee5f1794a
xiaowang <xiaowang@microsoft.com>
2021-11-09 15:42:17 +0800

    recording audio

--- app/src/main/java/com/example/samsungdemo/MainActivity.kt
+++ app/src/main/java/com/example/samsungdemo/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.samsungdemo
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.samsungdemo.databinding.ActivityMainBinding
 
+@SuppressLint("MissingPermission")
 class MainActivity : AppCompatActivity() {
-
-    private var audioRecord: AudioRecord = AudioRecord(
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private val audioRecord: AudioRecord by lazy {
+        AudioRecord(
             MediaRecorder.AudioSource.VOICE_RECOGNITION,
             16000,
             AudioFormat.CHANNEL_IN_MONO,
             AudioFormat.ENCODING_PCM_16BIT,
             3200
-    )
+        )
+    }
+    private val audioPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+        if (granted) {
+            audioRecord.startRecording()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        activityMainBinding.button.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                audioPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+                return@setOnClickListener
+            }
+
+            audioRecord.startRecording()
+        }
     }
 }
